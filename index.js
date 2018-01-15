f2f53e6e191af00bbc4e68c507fa28ec9bf2ce4d
steventh <steve.thomas@amdocs.com>
2018-01-15 13:45:01 -0500

    no need to export test files ... removing VerticalFilterBarTest from index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import VerticalFilterBarTest from 'test/VerticalFilterBarTest.jsx';
 import VerticalFilterBar from 'verticalFilterBar/VerticalFilterBar.jsx';
 import 'resources/_verticalFilterBar.scss';
-import 'test/_verticalFilterBarTest.scss';
 
 export {
   VerticalFilterBar,
-  VerticalFilterBarTest
 };
