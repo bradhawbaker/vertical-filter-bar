1057d38d91f74aaf7e493ec6033e62e4d57474c3
Arul.Nambi <arul.nambi@amdocs.com>
2018-11-08 11:52:15 -0500

    Improving the build performance and also renaming the bundle to have its uique name

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var webpack = require('webpack');
 var path = require('path');
-var devPort = process.env.PORT || 8005;
 
 var PATHS = {
   SRC: path.resolve(__dirname, '.'),
@@ -8,12 +7,17 @@ var PATHS = {
 };
 
 var config = {
-  entry: [path.resolve(PATHS.SRC, 'index.js')],
+  entry: { 
+    'vertical-filter-bundle': [
+      path.resolve(PATHS.SRC, 'index.js')
+    ]
+  },
   output: {
     path: PATHS.TARGET,
-    filename: 'bundle.js',
+    filename: '[name].js',
     library: 'vertical-filter-bar',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
+    sourceMapFilename: '[name].js.map'
   },
   resolve: {
     root: [path.resolve('.')]
@@ -36,19 +40,13 @@ var config = {
       {test: /\.(css|scss)$/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap']}
     ]
   },
-  devServer: {
-    port: devPort,
-    historyApiFallback: true,
-    publicPath: `http://localhost:${devPort}/vertical-filter-bar/`,
-    contentBase: PATHS.TARGET,
-    hot: true,
-    progress: true,
-    inline: true,
-    debug: true,
-    stats: {
-      colors: true
-    }
-  }
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+  ]
 };
 
 module.exports = config;
